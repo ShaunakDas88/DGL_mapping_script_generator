58263e8d66d75b5e819eedff668e65adc556a3f0
ShaunakDas88 <shaunakdas88@gmail.com>
2017-12-14 12:41:02 -0800

    rename script

--- generate_mapping_script.py
+++ generate_mapping_script.py
@@ -9,6 +9,10 @@ parser.add_option("--edge_distribution", dest="edge_distribution", default="poin
 parser.add_option("--mapping_script_file_name", dest="mapping_script_file", default="mapping-script.groovy", help="Where to create DGL mapping script file")
 (options, args) = parser.parse_args()
 
+if not options.schema_file_path:
+	print "\nPlease provide path to an existing valid schema file.\n"
+	exit()	
+
 schema_processor = SchemaProcessor(options.schema_file_path)
 propertykey_map, vertexlabel_map, edgelabel_map = schema_processor.build_schema_maps()
 mapper_generator = MapperGenerator(options.mapping_script_file, vertexlabel_map, edgelabel_map)
